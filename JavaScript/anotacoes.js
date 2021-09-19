f0dabe286de519d792811acc227247860594af86
M1113r <g234930@dac.unicamp.br>
2021-09-18 21:37:11 -0300

    ok

---
+++ JavaScript/anotacoes.js
@@ -0,0 +1,11 @@
+/*
+Tipos de variáveis:
+- string = letras
+- number = numericos
+- boolean = true/false.
+
+var curso, var Curso, var CURSO = são duas variaveis diferentes.
+
+JavaScript é case sensitive, pois ele diferencia maiusculo de minusculo.
+
+*/
---
+++ JavaScript/meu_script.js
@@ -0,0 +1,12 @@
+
+var texto = "Curso de JavaScript";
+
+var numeroInteiro = 7;
+
+var numeroFlutuante = 12.02;
+
+var boolean = false;
+
+document.write('<h1> Titulo </h1>')
+
+console.log("7");
